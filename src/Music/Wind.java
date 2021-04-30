40f972743312ee2cc140033a2f480f1aa8027ff6
cmz5480 <3547301360@qq.com>
2021-04-30 22:52:13 +0800

    Initial commit

---
+++ src/Music/Wind.java
@@ -0,0 +1,14 @@
+package Music;
+
+public class Wind extends Instrument{
+    //重写接口方法
+    public void play(Note n){
+        System.out.println("Wind.play()"+n);
+    }
+    String what() {
+        return "Wind"; }
+    void adjust() {
+        System.out.println("Adjusting Wind"); }
+}
+
+
